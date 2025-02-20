88643a16a04dd8edda4ca9083e41aeebd1651af8
unknownproperty <igor.buksheff@yandex.ru>
2025-02-21 01:58:25 +0300

    feat: remove apps and add xi.web

---
+++ apps/xi.web/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Button } from '@xipkg/button';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'auto',
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
