47ce19f5d3ca5129c6e983b62abc1a0d3ed6fc72
d9m0n4 <ched08@yandex.ru>
2025-05-07 15:07:07 +0300

    feat(49560475): add common.utils package

---
+++ packages/common.utils/index.ts
@@ -0,0 +1,4 @@
+export { useFullScreen } from './src/useFullScreen';
+export { useKeyPress } from './src/useKeyPress';
+export { useGetUrlWithParams } from './src/useGetUrlWithParams';
+export { useMedia } from './src/useMedia';
