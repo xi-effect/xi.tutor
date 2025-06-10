2a5e0ae612eaffdeaf33d493cd232e1f899bcda8
marina-bul <bragina_marina@mail.ru>
2025-06-10 08:35:26 +0300

    feat(50629805): Add students and subjects selects

--- packages/features.invoice/src/ui/InvoiceModal/InvoiceModal.tsx
+++ packages/features.invoice/src/ui/InvoiceModal/InvoiceModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Modal,
   ModalContent,
@@ -11,12 +12,32 @@ import { Input } from '@xipkg/input';
 import { Button } from '@xipkg/button';
 import { Close } from '@xipkg/icons';
 
+import { students } from '../../mocks';
+import type { StudentT, SubjectT } from '../../types/InvoiceTypes';
+
 type InvoiceModalProps = {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 };
 
 export const InvoiceModal = ({ open, onOpenChange }: InvoiceModalProps) => {
+  const [activeStudent, setActiveStudent] = useState<StudentT | null>(null);
+  const [activeSubject, setActiveSubject] = useState<SubjectT | null>(null);
+
+  const handleChangeStudent = (id: string) => {
+    setActiveStudent(students.find((student) => student.id === id) || null);
+  };
+
+  const handleChangeSubject = (id: string) => {
+    setActiveSubject(activeStudent?.subjects.find((subject) => subject.id === id) || null);
+  };
+
+  const handleCancel = () => {
+    setActiveStudent(null);
+    setActiveSubject(null);
+    onOpenChange(false);
+  };
+
   return (
     <Modal open={open} onOpenChange={onOpenChange}>
       <ModalContent className="w-[800px]">
@@ -36,24 +57,42 @@ export const InvoiceModal = ({ open, onOpenChange }: InvoiceModalProps) => {
 
           <div className="pb-6">
             <p>Ученик или группа</p>
-            <Select>
+            <Select value={activeStudent?.id} onValueChange={(value) => handleChangeStudent(value)}>
               <SelectTrigger className="w-full">
                 <SelectValue />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="Petrov">Алексей Петров</SelectItem>
-                <SelectItem value="group">Группа 10А</SelectItem>
+                {students.map((student) => (
+                  <SelectItem key={student.id} value={student.id}>
+                    {student.name}
+                  </SelectItem>
+                ))}
               </SelectContent>
             </Select>
           </div>
           <div className="pb-6">
-            <Input placeholder="Сумма" />
+            <p>Предметы</p>
+            <Select value={activeSubject?.id} onValueChange={(value) => handleChangeSubject(value)}>
+              <SelectTrigger className="w-full">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                {activeStudent?.subjects.map((subject) => (
+                  <SelectItem key={subject.id} value={subject.id}>
+                    {subject.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="pb-6">
+            <Input placeholder="Сумма" value={activeSubject?.price} />
           </div>
         </div>
 
         <ModalFooter className="border-gray-20 flex gap-2 border-t">
           <Button size="m">Создать</Button>
-          <Button size="m" variant="secondary" onClick={() => onOpenChange(false)}>
+          <Button size="m" variant="secondary" onClick={handleCancel}>
             Отменить
           </Button>
         </ModalFooter>
