f59eb434da9fe990ae885d528da35e4e9889c110
reinaka <juliabednaia@gmail.com>
2025-06-30 18:11:05 +0300

    fix(49695322): fix query client issues

--- packages/common.config/src/api/query.client.ts
+++ packages/common.config/src/api/query.client.ts
@@ -8,10 +8,8 @@ export const queryClient = new QueryClient({
     queries: {
       networkMode: 'offlineFirst',
       refetchOnWindowFocus: false,
-      refetchOnMount: true,
-      staleTime: 1000 * 60 * 5, // 5 minutes (уменьшено с 30 минут)
-      retry: 1, // Позволяем одну попытку повтора
-      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+      staleTime: 1000 * 60 * 30, // 30 minutes
+      retry: false,
     },
   },
 });
