c8d2b7ef12a63b3bc3134ba648d5fbfd89d6df47
xyzelena <emkhuzhina@gmail.com>
2025-07-01 13:06:29 +0530

    fix(51665993): enhance materials creation with success and error handling

--- packages/common.services/src/materials/useAddMaterials.tsx
+++ packages/common.services/src/materials/useAddMaterials.tsx
@@ -2,6 +2,7 @@ import { materialsApiConfig, MaterialsQueryKey } from 'common.api';
 import { getAxiosInstance } from 'common.config';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { generateNameWithDate } from 'common.utils';
+import { handleError, showSuccess } from 'common.services';
 
 export type MaterialsDataT = {
   kind: 'note' | 'board';
@@ -33,8 +34,15 @@ export const useAddMaterials = () => {
         throw err;
       }
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [MaterialsQueryKey.AddMaterials] });
+    onSuccess: (response) => {
+      if (response?.data) {
+        queryClient.invalidateQueries({ queryKey: [MaterialsQueryKey.AddMaterials] });
+      }
+
+      showSuccess('materials', `${response.data.name} создан`);
+    },
+    onError: (err) => {
+      handleError(err, 'materials');
     },
   });
 
