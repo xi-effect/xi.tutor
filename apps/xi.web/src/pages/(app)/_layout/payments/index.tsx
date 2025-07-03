d5f40258eaff34778eda852a643285c1791654b3
unknownproperty <igor.buksheff@yandex.ru>
2025-07-03 21:46:21 +0300

    fix: layouts for payments

--- apps/xi.web/src/pages/(app)/_layout/payments/index.tsx
+++ apps/xi.web/src/pages/(app)/_layout/payments/index.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { createFileRoute } from '@tanstack/react-router';
-import { PaymentControl as Payments } from 'features.charts';
+import { PaymentsPage } from 'pages.payments';
+
+const Payments = () => {
+  return <PaymentsPage />;
+};
 
 // @ts-ignore
 export const Route = createFileRoute('/(app)/_layout/payments/')({
-  component: PaymentsPage,
+  component: Payments,
   beforeLoad: ({ context, location }) => {
     console.log('Payments', context, location);
   },
 });
-
-function PaymentsPage() {
-  return <Payments />;
-}
