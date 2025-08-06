cf79293b12283a400e40056a4886cba0247407f8
adamov.s <adamov.s@ati.su>
2025-08-07 01:17:31 +0300

    fix(53736617): fix visual editor bugs

---
+++ packages/modules.editor/src/config/editorProps.ts
@@ -0,0 +1,10 @@
+export const editorProps = {
+  attributes: {
+    class:
+      'focus:outline-none focus:ring-0 focus:ring-offset-0 min-h-[300px] ' +
+      '[&>*+*]:mt-3 [&_ul]:pl-6 [&_ol]:pl-6 ' +
+      '[&_h1]:text-4xl [&_h1]:font-bold [&_h2]:text-3xl [&_h2]:font-semibold ' +
+      '[&_pre]:bg-gray-800 [&_pre]:text-white [&_pre]:p-4 [&_pre]:rounded ' +
+      '[&_blockquote]:border-l-4 [&_blockquote]:border-gray-300 [&_blockquote]:pl-4',
+  },
+};
