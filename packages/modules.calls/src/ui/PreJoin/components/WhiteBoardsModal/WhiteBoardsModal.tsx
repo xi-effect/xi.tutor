4340330efc70dfd5cde23146a9f6a209dc062c78
Gleb Batyan <66956508+Batlay@users.noreply.github.com>
2025-08-07 11:19:09 +0300

    feat(53678424): update modal UI and simplify debounce

--- packages/modules.calls/src/ui/PreJoin/components/WhiteBoardsModal/WhiteBoardsModal.tsx
+++ packages/modules.calls/src/ui/PreJoin/components/WhiteBoardsModal/WhiteBoardsModal.tsx
@@ -3,17 +3,19 @@ import { Search, WhiteBoard } from '@xipkg/icons';
 import { Input } from '@xipkg/input';
 import { ScrollArea } from '@xipkg/scrollarea';
 import { useDebounce } from '@xipkg/utils';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import {
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalDescription,
   ModalFooter,
   ModalHeader,
   ModalTitle,
   ModalTrigger,
 } from '@xipkg/modal';
+import { WhiteBoardItem } from './WhiteBoardItem';
 
 const mockWhiteBoardList = [
   {
@@ -35,48 +37,38 @@ const mockWhiteBoardList = [
     name: 'Доска 2',
   },
   {
-    name: 'Доска 5',
+    name: 'Доска 1',
   },
   {
-    name: 'Новая доска 2',
+    name: 'Новая доска',
   },
   {
-    name: 'Доска 6',
+    name: 'Доска 3',
   },
   {
-    name: 'Доска 7',
-  },
-  {
-    name: 'Доска 8',
+    name: 'Доска 4',
   },
   {
     name: 'Математика',
   },
+  {
+    name: 'Доска 2',
+  },
 ];
 
 export const WhiteBoardsModal = () => {
-  const [filteredBoards, setFilteredBoards] = useState(mockWhiteBoardList);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchInput, setSearchInput] = useState('');
   const [isOpen, setIsOpen] = useState(false);
 
-  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+  const searchName = useDebounce(searchInput, 300);
 
-  useEffect(() => {
-    if (debouncedSearchTerm) {
-      setFilteredBoards(
-        mockWhiteBoardList.filter((board) =>
-          board.name.toLowerCase().includes(debouncedSearchTerm.toLowerCase()),
-        ),
-      );
-    } else {
-      setFilteredBoards(mockWhiteBoardList);
-    }
-  }, [debouncedSearchTerm]);
+  const filteredBoards = mockWhiteBoardList.filter((board) =>
+    searchName ? board.name.toLowerCase().includes(searchName.toLowerCase()) : true,
+  );
 
   const handleCancel = () => {
     setIsOpen(false);
-    setSearchTerm('');
-    setFilteredBoards(mockWhiteBoardList);
+    setSearchInput('');
   };
 
   return (
@@ -98,34 +90,31 @@ export const WhiteBoardsModal = () => {
           <ModalTitle className="pr-10 sm:pr-0">Доска для совместной работы</ModalTitle>
           <Input
             placeholder="Поиск"
-            before={<Search className="text-gray-60 h-4 w-4" />}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            value={searchTerm}
+            before={<Search className="h-6 w-6" />}
+            onChange={(e) => setSearchInput(e.target.value)}
+            value={searchInput}
           />
           <ModalCloseButton variant="default" onClick={handleCancel} />
+          <ModalDescription />
         </ModalHeader>
         <ModalBody>
-          <ScrollArea className="max-h-66.5 min-h-43 w-full rounded-md">
-            <div className="flex flex-wrap gap-4">
-              <div className="bg-brand-0 flex h-19.5 w-full items-center justify-center rounded-2xl md:w-50">
+          <ScrollArea className={`max-h-67 ${filteredBoards.length > 9 && 'pr-4'}`}>
+            <div className="grid grid-cols-1 items-start gap-4 sm:grid-cols-2 lg:grid-cols-3">
+              {filteredBoards.map((board, index) => (
+                <WhiteBoardItem name={board.name} key={index} />
+              ))}
+              <div className="bg-brand-0 hover:border-brand-60 col-span-1 flex h-19.5 items-center justify-center rounded-2xl hover:border">
                 <span className="text-brand-100 text-s-base">Создать новую</span>
               </div>
-              {filteredBoards.map((board, index) => (
-                <div
-                  key={index}
-                  className="border-gray-40 flex h-19.5 w-full flex-col items-start justify-start gap-2 rounded-2xl border p-3 md:w-50"
-                >
-                  <h3 className="text-m-base font-medium text-gray-100">{board.name}</h3>
-                  <span className="text-xs-base text-gray-80 font-medium">Изменено: 5 августа</span>
-                </div>
-              ))}
             </div>
           </ScrollArea>
         </ModalBody>
         <ModalFooter footerVariant="default">
           <div className="flex gap-4">
-            <Button type="submit">Выбрать</Button>
-            <Button variant="secondary" onClick={handleCancel}>
+            <Button type="submit" className="w-29.25">
+              Выбрать
+            </Button>
+            <Button variant="secondary" onClick={handleCancel} className="w-27.25">
               Отмена
             </Button>
           </div>
