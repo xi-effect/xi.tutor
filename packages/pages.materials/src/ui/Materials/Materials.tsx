dd84f1374097377c252f1b086cfe6ee8ba8f1199
zex1l <krivorukov2007@mail.ru>
2025-08-28 15:33:07 +0300

    fix(54670837): fix render container items

--- packages/pages.materials/src/ui/Materials/Materials.tsx
+++ packages/pages.materials/src/ui/Materials/Materials.tsx
@@ -14,44 +14,47 @@ export const Materials = () => {
 
   return (
     <div ref={parentRef} className="h-[calc(100vh-158px)] overflow-auto">
-      {notFoundItems && <NotFoundItems text="Здесь пока нет досок" />}
-      <div
-        style={{
-          height: `${rowVirtualizer.getTotalSize()}px`,
-          width: '100%',
-          position: 'relative',
-        }}
-      >
-        {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-          const start = virtualRow.index * colCount;
-          const rowItems = items.slice(start, start + colCount);
+      {notFoundItems ? (
+        <NotFoundItems text="Здесь пока нет досок" />
+      ) : (
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+            const start = virtualRow.index * colCount;
+            const rowItems = items.slice(start, start + colCount);
 
-          return (
-            <div
-              key={virtualRow.index}
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                transform: `translateY(${virtualRow.start}px)`,
-                display: 'grid',
-                gap: GAP,
-                padding: GAP,
-                paddingLeft: 0,
-                boxSizing: 'border-box',
-                gridTemplateColumns: `repeat(${colCount}, minmax(0, 1fr))`,
-              }}
-            >
-              {rowItems.map((material) => (
-                <div key={material.id} className="card-item">
-                  <Card {...material} />
-                </div>
-              ))}
-            </div>
-          );
-        })}
-      </div>
+            return (
+              <div
+                key={virtualRow.index}
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  transform: `translateY(${virtualRow.start}px)`,
+                  display: 'grid',
+                  gap: GAP,
+                  padding: GAP,
+                  paddingLeft: 0,
+                  boxSizing: 'border-box',
+                  gridTemplateColumns: `repeat(${colCount}, minmax(0, 1fr))`,
+                }}
+              >
+                {rowItems.map((material) => (
+                  <div key={material.id} className="card-item">
+                    <Card {...material} />
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
