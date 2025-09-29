894231ed295a436c6801e1f657bd0127253e38d0
unknownproperty <igor.buksheff@yandex.ru>
2025-09-29 13:57:14 +0300

    feat(features.students.list): show delete icons

--- packages/features.students.list/src/ui/ModalStudentsList.tsx
+++ packages/features.students.list/src/ui/ModalStudentsList.tsx
@@ -105,7 +105,7 @@ export const ModalStudentsList = ({ children }: { children: React.ReactNode }) =
                           <TooltipTrigger asChild>
                             <Button
                               onClick={() => handleDeleteMode(student.tutorship.student_id)}
-                              className="bg-gray-5 hover:bg-gray-0 ml-auto hidden size-8 rounded-lg p-0 group-hover:flex"
+                              className="bg-gray-5 hover:bg-gray-0 ml-auto size-8 rounded-lg p-0"
                               variant="ghost"
                               size="s"
                             >
