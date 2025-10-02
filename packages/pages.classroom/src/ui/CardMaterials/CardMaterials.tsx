224226bc49576aa8110caa54aedc190af85ce76e
xyzelena <emkhuzhina@gmail.com>
2025-10-02 14:09:00 +0300

    feat(29): refactor props to include typeWork and typeMaterial

--- packages/pages.classroom/src/ui/CardMaterials/CardMaterials.tsx
+++ packages/pages.classroom/src/ui/CardMaterials/CardMaterials.tsx
@@ -1,13 +1,15 @@
 import { Badge } from '@xipkg/badge';
 import { formatToShortDate } from 'pages.materials';
 import { cn } from '@xipkg/utils';
+import { LongAnswer, WhiteBoard } from '@xipkg/icons';
 
 export type CardMaterialsProps = {
   value: {
     id: number;
     name: string;
     date: string;
-    type?: 'collaboration' | 'only_tutor' | 'draft';
+    typeWork?: 'collaboration' | 'only_tutor' | 'draft';
+    typeMaterial?: 'text' | 'board';
   };
 };
 
@@ -23,27 +25,38 @@ const mapStyles = {
   draft: 'bg-violet-20 text-violet-100',
 };
 
+const iconClassName = 'size-6 fill-gray-100';
+
+const mapIcon = {
+  text: <LongAnswer className={iconClassName} />,
+  board: <WhiteBoard className={iconClassName} />,
+};
+
 export const CardMaterials = ({ value }: CardMaterialsProps) => {
-  const { name, date, type } = value;
+  const { name, date, typeWork = 'draft', typeMaterial = 'text' } = value;
 
   return (
     <div className="border-gray-30 bg-gray-0 flex min-h-[96px] min-w-[350px] flex-col items-start justify-start gap-2 rounded-2xl border p-4">
-      {type && (
+      {typeWork && (
         <Badge
           variant="default"
           className={cn(
             'text-s-base px-2 py-1 font-medium',
-            mapStyles[type as keyof typeof mapStyles],
+            mapStyles[typeWork as keyof typeof mapStyles],
           )}
         >
-          {type && mapType[type as keyof typeof mapType]}
+          {typeWork && mapType[typeWork as keyof typeof mapType]}
         </Badge>
       )}
 
       <div className="flex flex-col items-start justify-start gap-4">
-        <h3 className="text-l-base font-medium text-gray-100">{name}</h3>
         <div className="flex flex-row items-center justify-start gap-2">
-          <span className="text-s-base text-gray-80 font-medium">
+          {typeMaterial && mapIcon[typeMaterial as keyof typeof mapIcon]}
+          <div className="text-l-base font-medium text-gray-100">{name}</div>
+        </div>
+
+        <div className="flex flex-row items-center justify-start gap-2">
+          <span className="text-s-base text-gray-60 font-normal">
             Изменено: {formatToShortDate(date)}
           </span>
         </div>
