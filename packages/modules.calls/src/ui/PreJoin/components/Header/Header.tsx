e44ceda1fa75005e383512a717dea94d0ebae20f
unknownproperty <igor.buksheff@yandex.ru>
2025-10-05 07:05:51 +0300

    feat(modules.call): add arrows to prejoin

--- packages/modules.calls/src/ui/PreJoin/components/Header/Header.tsx
+++ packages/modules.calls/src/ui/PreJoin/components/Header/Header.tsx
@@ -1,9 +1,37 @@
+import { useNavigate, useParams } from '@tanstack/react-router';
+import { Button } from '@xipkg/button';
+import { ArrowLeft } from '@xipkg/icons';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@xipkg/tooltip';
+import { useGetClassroom } from '../../../../../../common.services/src/classrooms/useGetClassroom';
+
 /* eslint-disable no-irregular-whitespace */
 export const Header = () => {
+  const navigate = useNavigate();
+  const { callId } = useParams({ strict: false }) as { callId: string };
+
+  const { data: classroom } = useGetClassroom(Number(callId));
+
   return (
-    <div className="mb-8 flex flex-row items-end gap-2">
+    <div className="mb-8 flex flex-row items-center gap-2">
+      <Tooltip delayDuration={1000}>
+        <TooltipTrigger asChild>
+          <Button
+            onClick={() => {
+              navigate({ to: '/classrooms/$classroomId', params: { classroomId: callId } });
+            }}
+            type="button"
+            variant="ghost"
+            className="flex size-[40px] items-center justify-center rounded-[12px] p-0"
+          >
+            <ArrowLeft className="fill-gray-100" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side="bottom" align="start">
+          Вернуться в кабинет
+        </TooltipContent>
+      </Tooltip>
       <h1 className="text-xl-base font-semibold text-gray-100">Присоединиться к занятию</h1>
-      {/* <p className="text-s-base text-gray-60 mb- align-baseline">Имя ученика/репетитора</p> */}
+      <p className="text-s-base text-gray-60 pt-2 align-baseline">{classroom?.name}</p>
     </div>
   );
 };
--- packages/modules.calls/src/ui/Up/UpBar.tsx
+++ packages/modules.calls/src/ui/Up/UpBar.tsx
@@ -90,7 +90,7 @@ export const UpBar = () => {
         <TooltipTrigger asChild>
           <Button
             onClick={() => {
-              navigate({ to: '/classrooms/$classroomId', params: { classroomId: '1' } });
+              navigate({ to: '/classrooms/$classroomId', params: { classroomId: callId } });
               updateStore('mode', 'compact');
             }}
             type="button"
