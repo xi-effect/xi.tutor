1a18d97b5402ca5d6001d51aecb39dd37e19281a
unknownproperty <igor.buksheff@yandex.ru>
2025-10-14 02:03:37 +0300

    feat(modules.calls): return prod mode

--- packages/modules.calls/src/utils/config.ts
+++ packages/modules.calls/src/utils/config.ts
@@ -1,5 +1,5 @@
 export const serverUrl = 'wss://livekit.sovlium.ru';
 export const serverUrlDev = 'ws://127.0.0.1:7880';
 export const devToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjAzOTIwNjAsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ0ZXN0LXVzZXIiLCJuYmYiOjE3NjAzOTE3NjAsInN1YiI6InRlc3QtdXNlciIsInZpZGVvIjp7InJvb20iOiJ0ZXN0LXJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.OyKoKgj7WIeejMHfJKv0AGr8icc62JO5Wz7h1WfEH90';
-export const isDevMode = true;
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjAzOTQwMDcsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ0ZXN0LXVzZXIiLCJuYmYiOjE3NjAzOTM3MDcsInN1YiI6InRlc3QtdXNlciIsInZpZGVvIjp7InJvb20iOiJ0ZXN0LXJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.ihf3fSwy-BLNt3oM2QlNw_Wuw7w3tc3vRDcKqM2OPRc';
+export const isDevMode = false;
