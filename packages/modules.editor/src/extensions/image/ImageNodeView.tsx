f9a35e48ea64958e5c3553ee412efb6cdd1184a5
unknown <ched08@yandex.ru>
2025-10-16 22:02:43 +0300

    fix(35): fix image size

--- packages/modules.editor/src/extensions/image/ImageNodeView.tsx
+++ packages/modules.editor/src/extensions/image/ImageNodeView.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NodeViewWrapper } from '@tiptap/react';
+import { NodeViewWrapper, NodeViewProps } from '@tiptap/react';
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -8,44 +8,41 @@ import {
   DropdownMenuSeparator,
 } from '@xipkg/dropdown';
 import { Button } from '@xipkg/button';
-import { ArrowBottom, ArrowUp, Copy, Download, MoreVert, Trash, Upload } from '@xipkg/icons';
-import clsx from 'clsx';
+import { ArrowBottom, ArrowUp, Copy, Download, MoreVert, Trash } from '@xipkg/icons';
 import { useBlockMenuActions, useYjsContext } from '../../hooks';
+import { cn } from '@xipkg/utils';
 
-type ImageNodeViewPropsT = {
-  node: any;
-  updateAttributes: (attrs: Record<string, any>) => void;
-  deleteNode: () => void;
-};
-
-export const ImageNodeView = ({ node }: ImageNodeViewPropsT) => {
+export const ImageNodeView = ({ node, selected }: NodeViewProps) => {
   const src = node.attrs.src;
   const { editor } = useYjsContext();
-
   const { duplicate, remove, downloadImage, moveDown, moveUp } = useBlockMenuActions(editor);
 
   return (
-    <NodeViewWrapper className="group relative max-w-[400px]">
-      <img src={src} alt={node.attrs.alt || ''} className={clsx('h-auto rounded-lg')} />
-      <div className="absolute top-2 right-2 flex opacity-0 group-hover:opacity-100">
+    <NodeViewWrapper className="group relative w-full transition-shadow">
+      <img
+        src={src}
+        alt={node.attrs.alt || ''}
+        className="h-auto w-full rounded-lg object-contain"
+      />
+
+      <div
+        className={cn(
+          'absolute top-2 right-2 flex transition-opacity',
+          selected ? 'opacity-100' : 'pointer-events-none opacity-0',
+        )}
+      >
         <DropdownMenu modal={false}>
           <DropdownMenuTrigger asChild>
             <Button size="s" variant="ghost" className="rounded-lg px-2">
               <MoreVert size="sm" />
             </Button>
           </DropdownMenuTrigger>
+
           <DropdownMenuContent
             side="bottom"
             align="end"
             className="flex w-[200px] flex-col space-y-1 p-2"
           >
-            <DropdownMenuItem
-              className="hover:bg-gray-5 h-7 gap-2 rounded p-1"
-              onSelect={() => console.log(1)}
-            >
-              <Upload size="sm" />
-              <span className="text-sm">Заменить</span>
-            </DropdownMenuItem>
             <DropdownMenuItem
               className="hover:bg-gray-5 h-7 gap-2 rounded p-1"
               onSelect={() => downloadImage(src)}
