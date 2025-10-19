ad1009e0c411775fb1a1ea6b78cc81638d2094f0
unknownproperty <igor.buksheff@yandex.ru>
2025-10-20 00:54:58 +0300

    fix(modules.calls): screen share

--- packages/modules.calls/src/utils/config.ts
+++ packages/modules.calls/src/utils/config.ts
@@ -1,5 +1,5 @@
 export const serverUrl = 'wss://livekit.sovlium.ru';
 export const serverUrlDev = 'ws://127.0.0.1:7880';
 export const devToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjA5MDYzNDAsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ0ZXN0LXVzZXIiLCJuYmYiOjE3NjA5MDYwNDAsInN1YiI6InRlc3QtdXNlciIsInZpZGVvIjp7InJvb20iOiJ0ZXN0LXJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.FR9hpOB8SNQMwCYQ69VtdjRAm9KNgFu6ex7xmhzeJsw';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjA5MDk3ODIsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ0ZXN0LXVzZXIiLCJuYmYiOjE3NjA5MDk0ODIsInN1YiI6InRlc3QtdXNlciIsInZpZGVvIjp7InJvb20iOiJ0ZXN0LXJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.Amvue_G_iBtViowcw0hSrnpLXlPl6Gr9nhodfhIq3Ls';
 export const isDevMode = true;
