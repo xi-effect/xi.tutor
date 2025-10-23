d56208caa1d0ad342c8597d1ff3c91e3b6300ef6
unknownproperty <igor.buksheff@yandex.ru>
2025-10-23 21:57:56 +0300

    feat(modules.calls): config to env

--- packages/modules.calls/src/utils/config.ts
+++ packages/modules.calls/src/utils/config.ts
@@ -1,5 +1,6 @@
-export const serverUrl = 'wss://livekit.sovlium.ru';
-export const serverUrlDev = 'ws://127.0.0.1:7880';
-export const devToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjA5MTI3MTYsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ0ZXN0LXVzZXIiLCJuYmYiOjE3NjA5MTI0MTYsInN1YiI6InRlc3QtdXNlciIsInZpZGVvIjp7InJvb20iOiJ0ZXN0LXJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.kocnDxrKazrduWczhefyunQFIPoDZwp4kFyqCE26Flk';
-export const isDevMode = false;
+import { env } from 'common.env';
+
+export const serverUrl = env.VITE_SERVER_URL_LIVEKIT;
+export const serverUrlDev = env.VITE_SERVER_URL_LIVEKIT_DEV;
+export const devToken = env.VITE_LIVEKIT_DEV_TOKEN;
+export const isDevMode = env.VITE_LIVEKIT_DEV_MODE;
