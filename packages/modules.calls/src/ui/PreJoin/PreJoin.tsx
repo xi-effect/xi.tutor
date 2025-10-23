4d9af88f7f05ff72ccc4b01f5f51d216474d0850
unknownproperty <igor.buksheff@yandex.ru>
2025-10-23 23:53:09 +0300

    fix(modules.calls): preJoin layout

--- packages/modules.calls/src/ui/PreJoin/PreJoin.tsx
+++ packages/modules.calls/src/ui/PreJoin/PreJoin.tsx
@@ -193,7 +193,7 @@ export const PreJoin = () => {
   return (
     <>
       <ScrollArea className="h-full w-full">
-        <div className="max-xs:p-4 p-8">
+        <div className="max-xs:p-4 p-4 pt-1">
           <Header />
           <div className="grid grid-cols-1 gap-8 xl:grid-cols-2">
             <UserTile audioTrack={audioTrack} videoTrack={videoTrack} />
