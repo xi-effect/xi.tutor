20dc61a9a2037171e7e70b94a021e96599b7d3f2
xyzelena <emkhuzhina@gmail.com>
2025-10-25 01:23:31 +0300

    feat(43): add icons dependency

--- packages/features.materials.add/src/ui/components/Board/Board.tsx
+++ packages/features.materials.add/src/ui/components/Board/Board.tsx
@@ -1,18 +1,28 @@
-import { Button } from '@xipkg/button';
+import { useState } from 'react';
 
+import { Button } from '@xipkg/button';
+import { ChevronSmallBottom, ChevronSmallTop } from '@xipkg/icons';
 interface BoardProps {
   onCreate: () => void;
 }
 
 export const Board = ({ onCreate }: BoardProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Button
-      onClick={onCreate}
       size="s"
       variant="secondary"
-      className="rounded-lg px-4 py-2 font-medium max-[550px]:hidden"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+      className="flex flex-row items-center gap-[6px] rounded-lg border border-gray-50 px-4 py-1 max-[550px]:hidden"
     >
-      Создать доску
+      <span className="text-s-base font-medium">Создать доску</span>
+      {isOpen ? (
+        <ChevronSmallTop className="fill-gray-0" />
+      ) : (
+        <ChevronSmallBottom className="fill-gray-100" />
+      )}
     </Button>
   );
 };
