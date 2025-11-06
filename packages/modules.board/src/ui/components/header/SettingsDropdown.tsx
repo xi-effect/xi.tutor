f051fbbe949706bfbd42a76b9cd09e7f17be89a4
xyzelena <emkhuzhina@gmail.com>
2025-11-07 00:13:12 +0300

    fix(58): update icon sizes in header, settings dropdown, hotkeys help, and navbar elements

--- packages/modules.board/src/ui/components/header/SettingsDropdown.tsx
+++ packages/modules.board/src/ui/components/header/SettingsDropdown.tsx
@@ -54,7 +54,7 @@ export const SettingsDropdown = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="h-[40px] w-[40px] p-2">
-          <MoreVert size="s" />
+          <MoreVert size="s" className="h-6 w-6" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent
