85880deb225daea6118266808bb0e671745a413b
unknownproperty <igor.buksheff@yandex.ru>
2025-11-06 22:58:42 +0300

    feat(xi.web): add glitchtip

--- apps/xi.web/src/main.tsx
+++ apps/xi.web/src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { i18nInitPromise } from './config/i18n';
+import { initGlitchTip } from './config/glitchtip';
 import { AppProviders } from './providers';
+
+// Инициализируем GlitchTip как можно раньше
+initGlitchTip();
 
 // Ждем инициализации i18n перед рендерингом приложения
 const rootElement = document.getElementById('root')!;
