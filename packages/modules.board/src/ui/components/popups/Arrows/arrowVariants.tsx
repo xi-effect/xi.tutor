7d311d84eeb4d719a03431514eccc279d4464594
unknown <ched08@yandex.ru>
2025-11-10 19:34:04 +0300

    fix(63): update icons package

--- packages/modules.board/src/ui/components/popups/Arrows/arrowVariants.tsx
+++ packages/modules.board/src/ui/components/popups/Arrows/arrowVariants.tsx
@@ -1,8 +1,8 @@
-import { Arrow, ArrowRight, RedLine } from '@xipkg/icons';
+import { Arrow, RedLine, ArrowDouble } from '@xipkg/icons';
 import { ArrowTypeT } from './types';
 
 export const arrowVariants: ArrowTypeT[] = [
   { name: 'line', icon: <RedLine />, end: 'none', start: 'none' },
   { name: 'arrow', icon: <Arrow />, end: 'none', start: 'arrow' },
-  { name: 'arrowDouble', icon: <ArrowRight />, end: 'arrow', start: 'arrow' },
+  { name: 'arrowDouble', icon: <ArrowDouble />, end: 'arrow', start: 'arrow' },
 ];
