3e4f68eaa7f524aa9a7daab87ea98ee6bbe4688f
xyzelena <emkhuzhina@gmail.com>
2025-11-13 00:38:22 +0300

    fix(60): remove generateNameWithDate utility function

--- packages/common.utils/index.ts
+++ packages/common.utils/index.ts
@@ -2,6 +2,5 @@ export { useFullScreen } from './src/useFullScreen';
 export { useKeyPress } from './src/useKeyPress';
 export { useGetUrlWithParams } from './src/useGetUrlWithParams';
 export { useMedia } from './src/useMedia';
-export { generateNameWithDate } from './src/generateNameWithDate';
 export { useNetworkStatus, NetworkProvider } from './src/NetworkContext';
 export { useRetryQueue } from './src/useRetryQueue';
