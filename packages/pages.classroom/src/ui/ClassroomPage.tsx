592218935a1e517ac256975a9a2f9abdbf45e992
reinaka <juliabednaia@gmail.com>
2025-12-28 16:47:44 +0300

    fix(88): change styles (payments/classroom)

--- packages/pages.classroom/src/ui/ClassroomPage.tsx
+++ packages/pages.classroom/src/ui/ClassroomPage.tsx
@@ -1,6 +1,3 @@
-import { Button } from '@xipkg/button';
-import { Plus } from '@xipkg/icons';
-
 import { Header } from './Header';
 import { Tabs } from './Tabs';
 
@@ -10,15 +7,6 @@ export const ClassroomPage = () => {
       <div className="flex flex-col pt-1">
         <Header />
         <Tabs />
-      </div>
-
-      <div className="xs:hidden flex flex-row items-center justify-end">
-        <Button
-          size="small"
-          className="fixed right-4 bottom-4 z-50 flex h-12 w-12 items-center justify-center rounded-xl"
-        >
-          <Plus className="fill-brand-0" />
-        </Button>
       </div>
     </div>
   );
