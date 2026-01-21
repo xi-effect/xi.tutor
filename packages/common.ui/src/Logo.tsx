ac7e5ee6f20df8ea6fcb45e1befe8dc1e0cb8658
unknownproperty <igor.buksheff@yandex.ru>
2026-01-21 06:54:27 +0300

    fix: logo

--- packages/common.ui/src/Logo.tsx
+++ packages/common.ui/src/Logo.tsx
@@ -1,20 +1,11 @@
+import lightLogo from '/assets/brand/navigationlogo-default-light.svg';
+import darkLogo from '/assets/brand/navigationlogo-default-dark.svg';
+
 export const Logo = ({ width = 135, height = 16 }: { width?: number; height?: number }) => {
   return (
     <>
-      <img
-        src="/assets/brand/navigationlogo-default-light.svg"
-        alt="logo"
-        width={width}
-        height={height}
-        className="dark:hidden"
-      />
-      <img
-        src="/assets/brand/navigationlogo-default-dark.svg"
-        alt="logo"
-        width={width}
-        height={height}
-        className="hidden dark:block"
-      />
+      <img src={lightLogo} alt="logo" width={width} height={height} className="dark:hidden" />
+      <img src={darkLogo} alt="logo" width={width} height={height} className="hidden dark:block" />
     </>
   );
 };
