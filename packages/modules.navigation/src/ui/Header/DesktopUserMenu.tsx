5be5b032713a45cd3538fecb36979eec33cb21c9
xyzelena <emkhuzhina@gmail.com>
2026-04-08 16:34:17 +0300

    fix(130): update styling for DesktopUserMenu and SelectRole components

--- packages/modules.navigation/src/ui/Header/DesktopUserMenu.tsx
+++ packages/modules.navigation/src/ui/Header/DesktopUserMenu.tsx
@@ -42,7 +42,7 @@ export const DesktopUserMenu = ({
       </DropdownMenuTrigger>
 
       <DropdownMenuContent side="bottom" align="end">
-        <div className="px-2 py-1.5">
+        <div className="text-gray-80 fill-gray-80 focus:bg-brand-0 hover:bg-brand-0 relative flex h-8 cursor-pointer items-center rounded px-2 py-1.5 text-sm outline-none select-none hover:fill-gray-100 hover:text-gray-100 data-disabled:pointer-events-none data-disabled:opacity-50">
           <SelectRole />
         </div>
 
