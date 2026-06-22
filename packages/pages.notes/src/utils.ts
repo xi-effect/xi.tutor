a18ade52c6be744e1dbbb9f17b74bc89b6fa9218
Avea-marina <mj20222020@gmail.com>
2026-06-22 21:32:02 +0300

    fix(pages.notes): fix env for base backend url

--- packages/pages.notes/src/utils.ts
+++ packages/pages.notes/src/utils.ts
@@ -1,5 +1,5 @@
-const AVATAR_API_BASE = 'https://api.sovlium.ru/files/users';
+import { env } from 'common.env';
 
 export const getAvatarUrlByUserId = (id: number): string | undefined => {
-  return `${AVATAR_API_BASE}/${id}/avatar.webp`;
+  return `${env.VITE_SERVER_URL_BACKEND}/files/users/${id}/avatar.webp`;
 };
