ad5f30ede13498d455d07098e7b73a4363a05978
Avea-marina <mj20222020@gmail.com>
2026-07-19 23:00:57 +0300

    fix(pages.classrooms): use GridVirtualizer component in xi.tutor

--- packages/pages.classrooms/src/hooks/index.ts
+++ packages/pages.classrooms/src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useInfiniteQuery } from './useInfiniteQuery';
 export { useInfiniteQueryStudent } from './useInfiniteQueryStudent';
 export { useScrollPagination } from './useScrollPagination';
-export { useResponsiveGrid } from './useResponsiveGrid';
 export { useVirtualGrid } from './useVirtualGrid';
