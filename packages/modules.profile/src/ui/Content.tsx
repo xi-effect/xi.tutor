8982c7a2806ac1f427cfeb639d97b35e2067b4e6
unknownproperty <igor.buksheff@yandex.ru>
2026-07-28 21:13:10 +0300

    fix(modules.profile): dark theme background for profile modal

--- packages/modules.profile/src/ui/Content.tsx
+++ packages/modules.profile/src/ui/Content.tsx
@@ -27,7 +27,7 @@ export const Content = ({ activeQuery }: ContentPropsT) => {
   const activeItem = componentMap[activeQuery] || <PersonalData />;
 
   return (
-    <div className="bg-gray-0 h-full min-h-0 w-full min-w-0 overflow-y-auto overscroll-contain pr-4">
+    <div className="bg-background-surface h-full min-h-0 w-full min-w-0 overflow-y-auto overscroll-contain pr-4">
       <div className="pb-4">{activeItem}</div>
     </div>
   );
